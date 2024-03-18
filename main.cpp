316619aafc94789ed47149b926729382cecffaf2
LiviuKHK <162412428+LiviuKHK@users.noreply.github.com>
2024-03-18 21:16:44 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -182,6 +182,40 @@ int main() {
     shaco.addAbility(ability3);
     shaco.addAbility(ability4);
 
+    cout << endl;
+
+    cout << "Information about Shaco:" << endl;
+    cout << "Name: " << shaco.getName() << endl;
+    cout << "Role: " << shaco.getRole() << endl;
+    cout << "Health: " << shaco.getHealth() << endl;
+    shaco.displayAbilities(); // Afișează abilitățile campionului Shaco
+    cout << endl;
+    cout << "Information about abilities:" << endl;
+    cout << "Name: " << ability3.getName() << endl;
+    cout << "Type: " << ability3.getType() << endl;
+    cout << "Damage: " << ability3.getDamage() << endl;
+    cout << "Cooldown: " << ability3.getCooldown() << endl;
+    cout << "Name: " << ability4.getName() << endl;
+    cout << "Type: " << ability4.getType() << endl;
+    cout << "Damage: " << ability4.getDamage() << endl;
+    cout << "Cooldown: " << ability4.getCooldown() << endl;
+    cout<<endl;
+    cout << "Information about Nocturne:" << endl;
+    cout << "Name: " << nocturne.getName() << endl;
+    cout << "Role: " << nocturne.getRole() << endl;
+    cout << "Health: " << nocturne.getHealth() << endl;
+    nocturne.displayAbilities(); // Afișează abilitățile campionului Shaco
+    cout << endl;
+    cout << "Information about abilities:" << endl;
+    cout << "Name: " << ability1.getName() << endl;
+    cout << "Type: " << ability1.getType() << endl;
+    cout << "Damage: " << ability1.getDamage() << endl;
+    cout << "Cooldown: " << ability1.getCooldown() << endl;
+    cout << "Name: " << ability2.getName() << endl;
+    cout << "Type: " << ability2.getType() << endl;
+    cout << "Damage: " << ability2.getDamage() << endl;
+    cout << "Cooldown: " << ability2.getCooldown() << endl;
+
     Champion champion1("Nocturne", "Assassin", 580);
 
     Champion champion2 = champion1; // Testare constructor de copiere;
@@ -200,12 +234,10 @@ int main() {
     cout << healthPotion << endl;
     cout << manaPotion << endl;
 
-    nocturne.displayAbilities();
-    shaco.displayAbilities();
-
     // Simulam o batalie dintre Shaco si Nocturne
     cout << "\nBattle Simulation:" << endl;
     shaco.simulateBattle(nocturne);
+    cout<<endl;
 
     return 0;
 
